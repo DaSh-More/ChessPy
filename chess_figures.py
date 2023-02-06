b67be391818526c6dc0a3e88a36aabcf3d4269b4
Никита Гришин <nikitagr1205@yandex.ru>
2023-02-06 19:53:48 +0300

    Created Pawn

--- chess_figures.py
+++ chess_figures.py
@@ -11,19 +11,39 @@ class Figure:
     def __init__(self, color: bool):
         '''
         Цвет задается при создании
+        белый = True
         '''
         self.color = color
+
     @abstractmethod
-    def possible_moves(cell: str) -> list:
+    def possible_moves(self, cell: list) -> list:
         ...
 
     @abstractmethod
-    def possible_takes(cell: str) -> list:
+    def possible_takes(self, cell: list) -> list:
         ...
+
 
 class Pawn(Figure):
     name = "Пешка"
     eng_name = "Pawn"
     notation_name = ""
     price = 1
-    
+
+    def possible_moves(self, cell: list) -> list:
+        if cell[1] in (0,7):
+            return []
+        if self.color:
+            if cell[1] == 1:
+                return [[cell[0], cell[1]+1], [cell[0], cell[1]+2]]
+            else:
+                return [[cell[0], cell[1]+1]]
+        else:
+            if cell[1] == 6:
+                return [[cell[0], cell[1]-1], [cell[0], cell[1]-2]]
+            else:
+                return [[cell[0], cell[1]-1]]
+
+
+a = Pawn(color=True)
+print(a.possible_moves([1, 7]))
